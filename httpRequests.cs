bea52c1e2d13dbb7a26511d8366e0d6e93a7bd4f
emotional-bug <hahafeelings@gmail.com>
2021-10-31 10:53:31 +0300

    Add a class for sending requests to the server

---
+++ httpRequests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace weather_api
+{
+    public class HttpRequests
+    {
+        static readonly HttpClient Client = new HttpClient();
+        
+        public async Task Execute()
+        {
+            try	
+            {
+                HttpResponseMessage response = await Client.GetAsync(Config.ApiToken);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+            }
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");	
+                Console.WriteLine("Message :{0} ",e.Message);
+            }
+        }
+    }
+}
